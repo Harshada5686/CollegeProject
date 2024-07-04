8438bbfca5811c6ced8f7e1f85305fe1791c4e51
PritishWa <92771064+PritishWa@users.noreply.github.com>
2024-07-04 20:54:36 +0530

    Task:pull updated code

--- html/store.php
+++ html/store.php
@@ -100,7 +100,6 @@
 <body>
     <section id="navigationBar">
         <nav class="navbar navbar-expand-lg ">
-            <!-- <a class="navbar-brand" href="#">BookHub</a> -->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
